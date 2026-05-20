5f790e99b2236c25c958b0e695afe2e350fb9343
Sarfaraj khan <khansarfaraj0403@gmail.com>
2026-05-20 10:44:50 +0530

    test-4

--- test_cal.py
+++ test_cal.py
@@ -1,10 +1,10 @@
 import pytest
 from calc import add,mul,sub,div
 def test_add():
-    assert(10,20) == 30
+    assert add(10,20) == 30
 def test_sub():
     assert sub(10, 5) == 5
 def test_mul():
     assert mul(10, 20) == 200
 def test_div():
-     assert div(10, 2) == 5
+    assert div(10, 2) == 5
